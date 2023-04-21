55b26b23b176d65b715af691f7e08a173c4652c9
GrommuL <miketsvg@gmail.com>
2023-04-21 12:23:08 +0600

    fix

--- app/js/main.js
+++ app/js/main.js
@@ -8,28 +8,8 @@ const popup = document.querySelector('.popup')
 const sideMenuLink = document.querySelectorAll('.popup__nav-title')
 const sideMenuLinkDropdown = document.querySelectorAll('.popup__nav-list')
 const closeBtn = document.querySelector('.popup__closebtn')
-const firstCard = document.querySelector('.partners__cards-item-first')
-const secondCard = document.querySelector('.partners__cards-item-second')
-const thirdCard = document.querySelector('.partners__cards-item-third')
 
-firstCard.addEventListener('click', ()=>{
-	firstCard.style.animation = 'slide .6s ease-in-out'
-	firstCard.style.zIndex = '8'
-	secondCard.style.zIndex = '10'
-	thirdCard.style.zIndex = '9'
-})
-secondCard.addEventListener('click', ()=>{
-	secondCard.style.animation = 'slide .6s ease-in-out'
-	secondCard.style.zIndex = '8'
-	thirdCard.style.zIndex = '10'
-	firstCard.style.zIndex = '9'
-})
-thirdCard.addEventListener('click', ()=>{
-	thirdCard.style.animation = 'slide .6s ease-in-out'
-	thirdCard.style.zIndex = '8'
-	firstCard.style.zIndex = '10'
-	secondCard.style.zIndex = '9'
-})
+
 burger.addEventListener('click',()=>{
 	burger.classList.toggle('open')
 	sideMenu.style.display = 'block'
